2ee4402898a769522df19987e2a69da34de5b00d
zahariaca <zaharia.c.alexandru@gmail.com>
2023-04-08 11:57:39 +0300

    Adds working broker-service, dockerized

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	_ = app.writeJson(w, http.StatusOK, payload)
+}
